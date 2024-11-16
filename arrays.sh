7ff1bfdd2271d4b82460f28c5dce96ef67fd1e6b
Timileyin <mosesekerin@gmail.com>
2024-11-16 19:22:17 +0000

    Added arrays.sh

---
+++ arrays.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#ARRAY=(One Two Three Four Five)
+
+#Index	0    1    2    3    4
+
+#echo ${ARRAY[@]}
+
+
+
+#Practice Test
+
+#1
+
+
+
+fruits=(apple banana cherry mango pineapple) 
+
+#echo ${#fruits[@]}
+
+echo ${fruits[0]}
+
+echo ${fruits[4]} 
+
